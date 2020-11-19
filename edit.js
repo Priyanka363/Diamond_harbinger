16fce81b8076701af383dff637cb8b73c38e85a6
Priyanka363 <priyamahawar261001@gmail.com>
2020-11-19 22:15:41 +0530

    adding flak and ml model file

--- edit.js
+++ edit.js
@@ -2,7 +2,7 @@ var nav1 = document.getElementById('navv');
 
 window.onscroll = function() {
 
-  if(window.pageYOffset >100) {
+  if(window.pageYOffset >80) {
     navv.style.background="white";
     navv.style.boxShadow="5px 2px 15px black";
   }
